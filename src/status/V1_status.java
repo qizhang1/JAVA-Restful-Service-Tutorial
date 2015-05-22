506445e2c356894f557c20d36f24328072299e72
Qi Zhang <qizhang1@uw.edu>
2015-05-22 15:10:50 -0700

    add api/v1/status/version

--- src/status/V1_status.java
+++ src/status/V1_status.java
@@ -2,12 +2,20 @@ package status;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/v1/status/*")
+@Path("/v1/status")
 public class V1_status {
 	
+	private static final String api_version = "00.01.00";
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnTitle(){
 		return "<p>Hello, Java Web Service</p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion(){
+		return "<p>Version: " + api_version + "</p>";
 	}
 }
