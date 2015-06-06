69c3bddd742fc8e9a71faa53fcd6ca468ab708ce
Qi Zhang <qizhang1@uw.edu>
2015-06-06 11:52:48 -0700

    add api/v2/orders/{product}/{quantity}

--- src/inventory/V2_Order.java
+++ src/inventory/V2_Order.java
@@ -67,7 +67,7 @@ public class V2_Order {
 	/**
 	 * Get all order details for a product.
 	 * This method uses the @PathParam to bring in the data. 
-	 * Ex: http://localhost:8080/com.youtube.rest/api/v2/orders/10
+	 * Ex: http://localhost:8080/SimpleRestAPI/api/v2/orders/10
 	 * 
 	 * @param productID
 	 * @return - json array results list from the database
@@ -93,6 +93,44 @@ public class V2_Order {
 					.build();
 		}
 
+		return Response.ok(returnString).build();
+	}
+	
+	/**
+	 * Search order on both productID and the product item number.
+	 * It uses @PathParam to bring in both parameters.
+	 * 
+	 * Ex:http://localhost:8080/SimpleRestAPI/api/v2/orders/10/30
+	 * 
+	 * @param productID
+	 * @param Quantity
+	 * @return - json array results list from the database
+	 * @throws Exception
+	 */
+	@Path("/{product}/{quantity}") // first parameter is usually the more general one, defines the scope
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnHighDemandProduct(
+				@PathParam("product") int productID,
+				@PathParam("quantity") int quantity) 
+				throws Exception {
+		
+		String returnString = null;
+		
+		JSONArray json = new JSONArray();
+		
+		try {
+			
+			SQLQueries db = new SQLQueries();
+			
+			json = db.queryOrderDetailsByProduct_Quantity(productID, quantity);
+			returnString = json.toString();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
 		return Response.ok(returnString).build();
 	}
 
