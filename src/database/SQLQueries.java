5c2a96c4ee88633d1ca56a7f17c5fe6e98e659dd
Qi Zhang <qizhang1@uw.edu>
2015-06-06 11:52:04 -0700

    add queryOrderDetailsByProduct_Quantity()

--- src/database/SQLQueries.java
+++ src/database/SQLQueries.java
@@ -7,9 +7,9 @@ import org.codehaus.jettison.json.JSONArray;
 import util.ToJSON;
 
 /**
- * This class will hold all the SQL queries Having all SQL/database code in one
- * package makes it easier to maintain and audit but increase complexity. Note:
- * This class extends SQLServer class to inherit all the methods
+ * This class will hold all the SQL queries 
+ * Having all SQL/database code in one package makes it easier to maintain and audit but increase complexity. 
+ * Note: This class extends SQLServer class to inherit all the methods
  * 
  * @author Qi Zhang
  */
@@ -30,6 +30,46 @@ public class SQLQueries extends SQLServer {
 					.prepareStatement("SELECT * FROM [ORDER DETAILS] WHERE PRODUCTID = ? ");
 
 			query.setInt(1, productID);
+			result = query.executeQuery();
+
+			json = converter.toJSONArray(result);
+			result.close();
+			query.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (result != null) result.close();
+			if (query != null) query.close();
+			if (conn != null) conn.close();
+		}
+
+		return json;
+	}
+
+	/**
+	 * Search for the orders for a specific productID with no less than given quantity from 
+	 * the Order Details table.
+	 * 
+	 * @param productID
+	 * @param quantity
+	 * @return - json array of the results from the database
+	 * @throws Exception
+	 */
+	public JSONArray queryOrderDetailsByProduct_Quantity(int productID, int quantity) throws Exception{
+		Connection conn = null;
+		PreparedStatement query = null;
+		ResultSet result = null;
+		
+		ToJSON converter = new ToJSON();
+		JSONArray json = new JSONArray();
+
+		try {
+			conn = SQLServerConnection();
+			query = conn
+					.prepareStatement("SELECT * FROM [ORDER DETAILS] WHERE PRODUCTID = ? AND QUANTITY >= ?");
+
+			query.setInt(1, productID);
+			query.setInt(2, quantity);
 			result = query.executeQuery();
 
 			json = converter.toJSONArray(result);
